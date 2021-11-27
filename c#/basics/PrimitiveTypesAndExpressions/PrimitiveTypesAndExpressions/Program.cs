9550d56dfe50e1060296dfe1c6fe039ed4d4023a
saurav dakre <saurav.dakre@gmail.com>
2021-11-27 20:46:42 +0530

    variables and constants

---
+++ c#/basics/PrimitiveTypesAndExpressions/PrimitiveTypesAndExpressions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello 1");
+            // variables and constants
+            int num1;
+            // Console.WriteLine(num1); // we cannot print unassigned variable
+
+            int n = 10;
+            float num2 = 20.5f; // explicitly tell compiler by appending f to number to tell its float
+            char character = 'q';
+            string str = "hello";
+            bool flag = false;
+            Console.WriteLine(n);
+            Console.WriteLine(num2);
+            Console.WriteLine(character);
+            Console.WriteLine(str);
+            Console.WriteLine(flag);
+
+            var n2 = 20.5;
+            Console.WriteLine(n2);
+
+            checked   // doesnot allow overflow at runtime, instead throw exception
+            {
+                byte num = 255;
+                Console.WriteLine(num+1);
+            }
+
+            Console.WriteLine("{0}, {1}", byte.MinValue, byte.MaxValue);
+
+            Console.WriteLine($"{float.MaxValue}, {float.MinValue}");
+
+            const float pi = 3.14f;
+            // pi = 1; cannot change value of const
+
+        }
+    }
+}
